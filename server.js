95d554d9651ca8c345e3c12687bb3cb233687dd9
roseak <rose.kohn@gmail.com>
2016-01-14 17:54:43 -0700

    Add ejs, a procfile, and links to see the admin and public polls.

--- server.js
+++ server.js
@@ -3,26 +3,40 @@ const express = require('express');
 const socketIo = require('socket.io');
 const app = express();
 const path = require('path');
+const crypto = require('crypto');
 
 const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+var polls = {}
+
 app.use(bodyParser.urlencoded({ extended:true }));
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.post('/', function(req, res){
+  poll = req.body.poll;
+  urlHash(poll);
+  var id = poll.id;
+  polls[id] = poll;
+  res.send("<div><a href='/" + poll.adminUrl + "/" + poll.id + "'>Admin URL</a><br><a href='/poll/" + poll.id + "'>Poll URL</a></div>");
+});
+
+app.get('/new', function(req, res){
+  res.sendFile(path.join(__dirname, '/public/new.html'));
 });
 
 app.get('/admin/*', function(req, res){
   res.sendFile(path.join(__dirname, '/public/admin.html'));
 });
 
-app.get('/poll/*', function(req, res){
-  res.sendFile(path.join(__dirname, '/public/poll.html'));
-});
-
-app.get('/new-poll', function(req, res){
-  res.sendFile(path.join(__dirname, '/public/new.html'));
+app.get('/poll/:id', function(req, res){
+  var poll = polls[req.params.id];
+  res.render('user-poll', { poll: poll });
 });
 
 var port = process.env.PORT || 3000;
@@ -34,8 +48,11 @@ var server = http.createServer(app)
 
 const io = socketIo(server);
 
-
-
+function urlHash(poll) {
+  poll.id = crypto.createHash('md5').update(poll.question + Date.now()).digest('hex');
+  poll.adminUrl = crypto.createHash('md5').update(poll.responses[0] + Date.now()).digest('hex');
+  return poll;
+}
 
 
 module.exports = server;
